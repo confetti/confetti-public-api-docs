35079519ee217a1f527d03a9c03a9a6680c55b91
Jonny Strömberg <jonny.stromberg@gmail.com>
2019-08-29 13:45:08 +0200

    Hello API docs! 👋

---
+++ app/api.js
@@ -0,0 +1,21 @@
+const API_KEY = '2-8eaa3a790e8b08076a74ab0cb19473134f84493990b9550a';
+const API_URL = 'https://api.confetti.events'
+
+const request = require('request-promise');
+const {Store} = require('yayson')()
+const store = new Store()
+
+module.exports = {
+  get: (path) => {
+    return request({
+      uri: API_URL + path,
+      headers: {
+        'authorization': 'apikey ' + API_KEY
+      },
+      json: true
+    })
+  },
+  format: (data) => {
+    return store.sync(data);
+  }
+}

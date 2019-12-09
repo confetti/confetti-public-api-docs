4ce0789bd71b1ad09a32e5931dd2f668450ffaed
Jonny Strömberg <jonny.stromberg@gmail.com>
2019-12-09 15:28:35 +0100

    Support custom api url and key

--- app/api.js
+++ app/api.js
@@ -1,5 +1,6 @@
-const API_KEY = '2-8eaa3a790e8b08076a74ab0cb19473134f84493990b9550a'
-const API_URL = 'https://api.confetti.events'
+const API_KEY =
+  process.env.API_KEY || '2-8eaa3a790e8b08076a74ab0cb19473134f84493990b9550a'
+const API_URL = process.env.API_URL || 'https://api.confetti.events'
 
 const request = require('request-promise')
 const { Store } = require('yayson')()
